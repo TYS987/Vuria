2357c2a8c86df667cbb724221d4f7a86691f1e93
TYS987 <139043546+TYS987@users.noreply.github.com>
2025-11-18 17:33:50 +0800

    改一下定时

--- lib/MoodKindeepCompanionemotional/MoodKinconnectionReactollector.dart
+++ lib/MoodKindeepCompanionemotional/MoodKinconnectionReactollector.dart
@@ -31,26 +31,26 @@ final Map<String, WidgetBuilder> appRoutes = {
         ? TrustedConfidantsWidget()
         : EmpathyExchangeWidget();
   },
-  '${'462aedb2aabf2526b3f8042304fca47d67448f06656d53106af8e00e8f58971d'.MoodKinfromPetalWhisper()}': (context) => EmpathyExchangeWidget(), //登录页面
+  '${'462aedb2aabf2526b3f8042304fca47d67448f06656d53106af8e00e8f58971d'.MoodKinfromPetalWhisper()}': (context) => EmpathyExchangeWidget(), 
 
-  '${'ea8e53d6c78d5553207089d7e7ff290d10c110db4429c456487a7c4744f2fd96'.MoodKinfromPetalWhisper()}': (context) => TrustedConfidantsWidget(), // 首页
-  '${'60868605b68de1bd4868a8ee27c11dd0'.MoodKinfromPetalWhisper()}': (context) => PendulumTrustWidget(), // 动态
-  '${'ab566411ea38260579abd47a2fc4a1909631d880804ca1bc6978858936273add'.MoodKinfromPetalWhisper()}': (context) => HoneycombUnderstandWidget(), // 个人主页
-  '${'7f9a7a0b9a1cbd7c7971f94ba37fbb95a218516f2861da9809c0ed1aaf12b58b'.MoodKinfromPetalWhisper()}': (context) => DnaEncryptedFeelingsWidget(), // 充值页面
+  '${'ea8e53d6c78d5553207089d7e7ff290d10c110db4429c456487a7c4744f2fd96'.MoodKinfromPetalWhisper()}': (context) => TrustedConfidantsWidget(),
+  '${'60868605b68de1bd4868a8ee27c11dd0'.MoodKinfromPetalWhisper()}': (context) => PendulumTrustWidget(), 
+  '${'ab566411ea38260579abd47a2fc4a1909631d880804ca1bc6978858936273add'.MoodKinfromPetalWhisper()}': (context) => HoneycombUnderstandWidget(),
+  '${'7f9a7a0b9a1cbd7c7971f94ba37fbb95a218516f2861da9809c0ed1aaf12b58b'.MoodKinfromPetalWhisper()}': (context) => DnaEncryptedFeelingsWidget(), 
   '${'644684279005c1ac544de5d4da40a855c071fad026da2fc9e5bcbdf305ac858d'.MoodKinfromPetalWhisper()}': (context) =>
-      FifthDimensionalLoveWidget(), // about us
-  '${'7411174b39d64bd8c67dae6232bb7c08bff6f6b4fd7baf8e774969a380a8a8e5'.MoodKinfromPetalWhisper()}': (context) => FragrantMemoriesWidget(), // 创建聊天房间页面
+      FifthDimensionalLoveWidget(),
+  '${'7411174b39d64bd8c67dae6232bb7c08bff6f6b4fd7baf8e774969a380a8a8e5'.MoodKinfromPetalWhisper()}': (context) => FragrantMemoriesWidget(), 
   '${'d0831261d414a358de3d17fbeaefcdf3903cf95a8020faabd828a96557775b66'.MoodKinfromPetalWhisper()}': (context) =>
-      InterstellarSolaceTearsWidget(), // 设置页面
-  '${'4bde32a8c6b074d06f7efa3fe66905406691a18f64994d047e292a68e777933f'.MoodKinfromPetalWhisper()}': (context) => MendingButterfliesWidget(), // AI 页面
-  '${'e1dc5011756527bd3cd8fbb6a5d06382deafce64fbbc398eb2fd98cd63e5c5fc'.MoodKinfromPetalWhisper()}': (context) => NeuralEmpathyPulseWidget(), // 发布动态页面
-  '${'6814c6f03f7848cb12c885cd6a06c54fbdde9d9648722e2513591547cee2707f'.MoodKinfromPetalWhisper()}': (context) => PhonographFeelsWidget(), // 举报页面
-  '${'dd3d72be965d0b39af73fc0fc0c42e147405765ce07c2ef627ef2ea3d60a71ea'.MoodKinfromPetalWhisper()}': (context) => SentimentMosaicWidget(), // 聊天列表
+      InterstellarSolaceTearsWidget(),
+  '${'4bde32a8c6b074d06f7efa3fe66905406691a18f64994d047e292a68e777933f'.MoodKinfromPetalWhisper()}': (context) => MendingButterfliesWidget(), 
+  '${'e1dc5011756527bd3cd8fbb6a5d06382deafce64fbbc398eb2fd98cd63e5c5fc'.MoodKinfromPetalWhisper()}': (context) => NeuralEmpathyPulseWidget(), 
+  '${'6814c6f03f7848cb12c885cd6a06c54fbdde9d9648722e2513591547cee2707f'.MoodKinfromPetalWhisper()}': (context) => PhonographFeelsWidget(), 
+  '${'dd3d72be965d0b39af73fc0fc0c42e147405765ce07c2ef627ef2ea3d60a71ea'.MoodKinfromPetalWhisper()}': (context) => SentimentMosaicWidget(), 
   '${'c98826c54e908b0d66490efcc65cf0d4e2cf5ac884b0a1372341b69e50c4cca9'.MoodKinfromPetalWhisper()}': (context) =>
-      SyntheticmpathybionicTrustWidget(), // Feedback 页面
-  '${'7c6061493daf514f35b35a87a0a83bf9b012f81cb53ee686cf9c654a18d6f71c'.MoodKinfromPetalWhisper()}': (context) => TimeDilationTherapyWidget(), // 修改个人信息页面
+      SyntheticmpathybionicTrustWidget(), 
+  '${'7c6061493daf514f35b35a87a0a83bf9b012f81cb53ee686cf9c654a18d6f71c'.MoodKinfromPetalWhisper()}': (context) => TimeDilationTherapyWidget(), 
 
-  // AI聊天页面
+
   '${'0809670819e3b0f424c90318f325f36006fa28d1b9ba5e0e95f78b14ae9d4062'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -58,7 +58,7 @@ final Map<String, WidgetBuilder> appRoutes = {
       sentimentSynchronizer: args?['${'012b24479c7433b71e9f076f30c97e735c60f09d7a3483de6858c636aff28863'.MoodKinfromPetalWhisper()}'] ?? '',
     );
   },
-  // 聊天房间页面
+
   '${'23c8f9fdf2b023027772ddf038f5afda793ce270a44ab4f99e93ca7bb0b46200'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -67,7 +67,7 @@ final Map<String, WidgetBuilder> appRoutes = {
     );
   },
 
-  // 拉黑粉丝关注页面
+
   '${'37a588393a0e4cc913ad566e9c2852820c0dd332c49b272453506d58bf7ec28d'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -76,7 +76,7 @@ final Map<String, WidgetBuilder> appRoutes = {
     );
   },
 
-  // 举报页面 sentimentAligned
+
   '${'b5657efd9615e6ffd837e08610bcb569ae593fe49c027079b7ba459680cbe185'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -84,7 +84,7 @@ final Map<String, WidgetBuilder> appRoutes = {
       moodStoryCanvas: args?['${'0e7a242c5949438c0169642fad0e9a69'.MoodKinfromPetalWhisper()}'] ?? '',
     );
   },
-  // 他人主页
+
   '${'014641a073e07a2615a7d36c057deef0cf5d5ad80e564f69eb3a47de34e15afb'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -92,7 +92,7 @@ final Map<String, WidgetBuilder> appRoutes = {
       empatheticStoryPortal: args?['${'7950da15ac1d74275297d2084b53f7a938d6c5e2c4e574661525195b65411177'.MoodKinfromPetalWhisper()}'] ?? '',
     );
   },
-  // 多人聊天房间页面
+
   '${'abca4747c63328b1c09ad9b9e85e9775a97a22bcfb1965aab816b8f467405f49'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -100,7 +100,7 @@ final Map<String, WidgetBuilder> appRoutes = {
       communitySentimentSync: args?['${'295f145dd91a8de74f0928d74eb931839c76730e890e2e2ce634bfb080e72bf6'.MoodKinfromPetalWhisper()}'] ?? '',
     );
   },
-  // 拨打视频
+
   '${'24e66feab0222fb41038fd68cd0bd4df'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -108,7 +108,7 @@ final Map<String, WidgetBuilder> appRoutes = {
       feelingSyncChanneoulConnec: args?['${'9b261316cb999e113e6b7dd70ed77ae9aef218ee8ef5455b0208fb7c117e375b'.MoodKinfromPetalWhisper()}'] ?? '',
     );
   },
-  // 动态详情页面
+
   '${'b71e5a4bc28a410497bbedc15c1fbdf22c0a7a8f82f16647f7a41e07cb8a25a0'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
@@ -116,7 +116,7 @@ final Map<String, WidgetBuilder> appRoutes = {
       soulConnectionMatrix: args?['${'2b3cd660daf164f9d7336075af994721835d89fd90949b1d35dda7c65229eff6'.MoodKinfromPetalWhisper()}'] ?? '',
     );
   },
-// 展示H5页面
+
   '${'542b5eba48e7c24a19780d01a3f8c5466d46d0fccd0cd4ad12e234fc2f31bac8'.MoodKinfromPetalWhisper()}': (context) {
     final args =
         ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
